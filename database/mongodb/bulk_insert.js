23fb9d7c97d8b0c8b152e868b9fb805a3f4ba933
Dina Berry <41597107+diberry@users.noreply.github.com>
2021-03-02 17:40:42 -0800

    MongoDB bulk insert

---
+++ database/mongodb/bulk_insert.js
@@ -0,0 +1,56 @@
+console.log(new Date())
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectID;
+require('dotenv').config();
+
+const fs = require('fs');
+const parse = require('csv-parser')
+const { finished } = require('stream/promises');
+
+const DATABASE_URL = process.env.DATABASE_URL
+    ? process.env.DATABASE_URL
+    : 'mongodb://localhost:27017';
+const DATABASE_NAME = process.env.DATABASE_NAME || 'my-tutorial-db';
+const DATABASE_COLLECTION_NAME =
+    process.env.DATABASE_COLLECTION_NAME || 'my-collection';
+
+let mongoConnection = null;
+let db = null;
+let collection = null;
+
+
+// insert each row into Redis
+const insertData = async (readable) =>{
+    
+    let i = 0;
+    
+    for await (const row of readable) {
+        console.log(`${i++} = ${JSON.stringify(row.goodreads_book_id)}`);
+        await collection.insertOne(row);
+    }
+}
+const bulkInsert = async () => {
+    
+    mongoConnection = await MongoClient.connect(DATABASE_URL, { useUnifiedTopology: true });
+    db = mongoConnection.db(DATABASE_NAME);
+    collection = await db.collection(DATABASE_COLLECTION_NAME);
+    
+    // read file, parse CSV, each row is a chunk
+    const readable = fs
+    .createReadStream('./books.csv')
+    .pipe(parse());
+
+    // Pipe rows to insert function
+    await insertData(readable)
+    await mongoConnection.close();
+}
+
+bulkInsert().then(() => {
+    console.log('done');
+    console.log(new Date())
+
+}).catch(err => {
+    console.log(`done +  failed ${err}`)
+    console.log(new Date())
+
+})
