bbe409f453922bce580912bf0fe910c65d92e3bc
Dina Berry <41597107+diberry@users.noreply.github.com>
2021-03-16 09:04:06 -0700

    JS - app settings

---
+++ apps/apps/download-certificate-orders.js
@@ -0,0 +1,21 @@
+const msRestNodeAuth = require("@azure/ms-rest-nodeauth");
+const { WebSiteManagementClient, We } = require("@azure/arm-appservice");
+
+// CHANGE THESE VALUES TO YOUR OWN
+// Either set environment variables 
+// or set your value to the string at the end
+const subscriptionId = process.env["MY-SUBSCRIPTION"] || "";
+const resourceGroupName = process.env["MY-RESOURCE-GROUP"] || "";
+const resourceName = process.env["MY-RESOURCE-NAME"] || "";
+
+const getCertificates = async (creds) => {
+    const client = new WebSiteManagementClient(creds, subscriptionId);
+    const certificateOrdersList = await client.appServiceCertificateOrders.list();
+    console.log(JSON.stringify(certificateOrdersList));
+}
+
+msRestNodeAuth.interactiveLogin().then((creds) => {
+    return getSettings(creds);
+}).catch((err) => {
+  console.error(err);
+});
