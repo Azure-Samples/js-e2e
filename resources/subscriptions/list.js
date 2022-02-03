3e283c62ad41fefc66d57f900bfbbfe89eea7007
Dina Berry <41597107+diberry@users.noreply.github.com>
2022-02-03 09:33:35 -0800

    fix environment var name

--- resources/subscriptions/list.js
+++ resources/subscriptions/list.js
@@ -7,8 +7,7 @@ const tenantId = process.env["AZURE_TENANT_ID"] || "REPLACE-WITH-YOUR-TENANT-ID"
 const clientId = process.env["AZURE_CLIENT_ID"] || "REPLACE-WITH-YOUR-CLIENT-ID"; 
 const secret = process.env["AZURE_CLIENT_SECRET"] || "REPLACE-WITH-YOUR-CLIENT-SECRET";
 
-
-if(process.env.production){
+if(process.env.NODE_ENV && process.env.NODE_ENV==='production'){
 
   // production
   credentials = new DefaultAzureCredential();
@@ -16,8 +15,11 @@ if(process.env.production){
 }else{
 
   // development
-  credentials = new ClientSecretCredential(tenantId, clientId, secret);
-  console.log("development");
+  if(tenantId && clientId && secret){
+    credentials = new ClientSecretCredential(tenantId, clientId, secret);
+  } else {
+    credentials = new DefaultAzureCredential();
+  }
 }
 
 // use credential to authenticate with Azure SDKs
